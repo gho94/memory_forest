eb3f0aec8365c3dbf9e76282b97d42c20a95ba33
Eunbi-Lee <lyb6336@naver.com>
2025-08-02 02:08:59 +0900

    fix: game master service를 impl 구조로 변경

--- backend/SpringBoot/src/main/java/com/bureung/memoryforest/game/application/GameMasterService.java
+++ backend/SpringBoot/src/main/java/com/bureung/memoryforest/game/application/GameMasterService.java
@@ -1,23 +1,10 @@
 package com.bureung.memoryforest.game.application;
 
 import com.bureung.memoryforest.game.domain.GameMaster;
-import com.bureung.memoryforest.game.repository.GameMasterRepository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
-@Service
-public class GameMasterService {
-    private final GameMasterRepository gameMasterRepository;
 
-    public GameMasterService(GameMasterRepository gameMasterRepository) {
-        this.gameMasterRepository = gameMasterRepository;
-    }
-
-    public List<GameMaster> getGamesByGameName(String gameName) {
-        return gameMasterRepository.findByGameNameContaining(gameName);
-    }
-
-    public GameMaster saveGame(GameMaster game) {
-        return gameMasterRepository.save(game);
-    }
+public interface GameMasterService {
+    List<GameMaster> getGamesByGameName(String gameName);
+    GameMaster saveGame(GameMaster game);
 }
