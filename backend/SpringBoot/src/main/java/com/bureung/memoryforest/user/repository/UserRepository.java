bc6ec99e978cd60cf882c577b94fbdba7d988dee
yiseul <kdewly715@gmail.com>
2025-08-03 23:42:41 +0900

    시큐리티 설정 1차

--- backend/SpringBoot/src/main/java/com/bureung/memoryforest/user/repository/UserRepository.java
+++ backend/SpringBoot/src/main/java/com/bureung/memoryforest/user/repository/UserRepository.java
@@ -1,4 +1,14 @@
+// package com.bureung.memoryforest.user.repository;
+
+// public class UserRepository {
+// }
 package com.bureung.memoryforest.user.repository;
 
-public class UserRepository {
+import com.bureung.memoryforest.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUserId(String userId);
 }
