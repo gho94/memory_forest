015d70a3e7f1aa3c74d63fcd471c70c2ef4535b7
yiseul <kdewly715@gmail.com>
2025-08-04 23:23:01 +0900

    시큐리티 다시 시작 예정-전체 코드 임시 주석처리

--- backend/SpringBoot/src/main/java/com/bureung/memoryforest/auth/security/CustomUserDetails.java
+++ backend/SpringBoot/src/main/java/com/bureung/memoryforest/auth/security/CustomUserDetails.java
@@ -1,35 +1,35 @@
-package com.bureung.memoryforest.auth.security;
+// package com.bureung.memoryforest.auth.security;
 
-import com.bureung.memoryforest.user.domain.User;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+// import com.bureung.memoryforest.user.domain.User;
+// import lombok.RequiredArgsConstructor;
+// import org.springframework.security.core.GrantedAuthority;
+// import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
-import java.util.Collections;
+// import java.util.Collection;
+// import java.util.Collections;
 
-@RequiredArgsConstructor
-public class CustomUserDetails implements UserDetails {
+// @RequiredArgsConstructor
+// public class CustomUserDetails implements UserDetails {
 
-    private final User user;
+//     private final User user;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singleton(() -> "ROLE_" + user.getUser_type());
-    }
+//     @Override
+//     public Collection<? extends GrantedAuthority> getAuthorities() {
+//         return Collections.singleton(() -> "ROLE_" + user.getUser_type());
+//     }
 
-    @Override
-    public String getPassword() {
-        return user.getPassword();
-    }
+//     @Override
+//     public String getPassword() {
+//         return user.getPassword();
+//     }
 
-    @Override
-    public String getUsername() {
-        return user.getUser_id(); // 또는 user.getEmail()
-    }
+//     @Override
+//     public String getUsername() {
+//         return user.getUser_id(); // 또는 user.getEmail()
+//     }
 
-    @Override public boolean isAccountNonExpired() { return true; }
-    @Override public boolean isAccountNonLocked() { return true; }
-    @Override public boolean isCredentialsNonExpired() { return true; }
-    @Override public boolean isEnabled() { return true; }
-}
+//     @Override public boolean isAccountNonExpired() { return true; }
+//     @Override public boolean isAccountNonLocked() { return true; }
+//     @Override public boolean isCredentialsNonExpired() { return true; }
+//     @Override public boolean isEnabled() { return true; }
+// }
