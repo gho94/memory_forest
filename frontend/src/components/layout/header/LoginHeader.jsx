18cbfcc56e5f4bc4a338dfd94572d765c620d717
Eunbi-Lee <lyb6336@naver.com>
2025-08-14 23:53:01 +0900

    feat: 가족/환자 footer & header back btn 연동

--- frontend/src/components/layout/header/LoginHeader.jsx
+++ frontend/src/components/layout/header/LoginHeader.jsx
@@ -3,7 +3,7 @@ import '@/assets/css/common.css';
 function LoginHeader({ title = '아이디 찾기' }) {
   return (
     <header className="app-header utility-header">
-      <div className="back-btn" role="button" aria-label="뒤로가기"></div>
+      <div onClick={() => window.history.back()} className="back-btn" role="button" aria-label="뒤로가기"></div>
       <div className="center-group">
         <div className="logo" aria-label="기억 숲 로고"></div>
         <div className="title">{title}</div>
--- frontend/src/components/layout/header/SignupHeader.jsx
+++ frontend/src/components/layout/header/SignupHeader.jsx
@@ -4,7 +4,7 @@ import '@/assets/css/login.css';
 function SignupHeader() {
   return (
     <header className="app-header utility-header">
-      <div href="#" className="back-btn"></div>
+      <div onClick={() => window.history.back()} className="back-btn"></div>
       <div className="center-group">
         <div className="logo" aria-label="기억 숲 로고"></div>
         <div className="title">회원가입</div>
