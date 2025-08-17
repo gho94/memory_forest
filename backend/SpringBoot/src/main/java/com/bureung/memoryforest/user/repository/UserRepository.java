a884425d8c7a332831c7b2d46dc3c1c3707e5134
Eunbi-Lee <lyb6336@naver.com>
2025-08-18 00:44:10 +0900

    feat: 환자 마이페이지 기능 추가 (프론트&백엔드 구현)

--- backend/SpringBoot/src/main/java/com/bureung/memoryforest/user/repository/UserRepository.java
+++ backend/SpringBoot/src/main/java/com/bureung/memoryforest/user/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.bureung.memoryforest.user.repository;
 
 import com.bureung.memoryforest.user.domain.User;
+import com.bureung.memoryforest.user.dto.response.UserRecorderResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -26,4 +28,13 @@ public interface UserRepository extends JpaRepository<User, String> {
 
     Optional<User> findByLoginIdAndEmail(String loginId, String email);
 
+    @Query(value = """
+    SELECT r.user_name as userName, r.gender_code as genderCode, DATE_FORMAT(r.birth_date, '%Y-%m-%d') as birthDate, 
+           p.user_name as companionName, p.phone as companionPhone, ur.relationship_code as relationshipCode
+    FROM users r 
+    INNER JOIN user_rel ur ON r.user_id = ur.patient_id
+    INNER JOIN users p ON p.user_id = ur.family_id
+    WHERE r.user_id = :userId
+    """, nativeQuery = true)
+    UserRecorderResponseDto findRecorderInfo(@Param("userId") String userId);
 }
